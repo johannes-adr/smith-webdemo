716cc342ca04344dcb91b72f174b5c4cb162d14a
johannes-adr <john.rans@web.de>
2023-04-30 17:24:52 +0200

    init

---
+++ main.js
@@ -0,0 +1,72 @@
+import init, {generate_ts, show_imps_map,generate_rust} from "./pkg/smith_webdemo.js"
+import hljs from 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.8.0/es/highlight.min.js';
+//  and it's easy to individually load additional languages
+import rust from 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.8.0/es/languages/rust.min.js';
+import typescript from 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.8.0/es/languages/typescript.min.js';
+hljs.registerLanguage('rust', rust);
+hljs.registerLanguage('typescript',typescript);
+
+let schema = `struct Pair<A,B>{
+    first:  A
+    second: B
+}
+
+enum Optional<T>{
+    Some(T)
+    None
+}
+
+enum WebEvent {
+    PageLoad
+    KeyPress(u8)
+    OnInput(Optional<WebEvent>)
+    Click(Pair<u32,u32>)
+}`
+
+let schemafield = (()=>{
+    let root = document.getElementById("schema");
+    let c = ()=>{
+    };
+    root.addEventListener("input", c)
+    root.value = schema
+    root.classList.add("codefield")
+    return {el: root,setValue:(v)=>{
+        root.innerText = v;
+        c()
+    }}
+})();
+
+
+let impsmap = createField("js")
+let rustcode = createField("rs");
+let tscode = createField("ts");
+
+function createField(type){
+    let root = document.createElement("pre");
+    let code = document.createElement("code");
+    code.classList.add("language-" + type);
+    root.classList.add("output")
+    root.append(code);
+
+    return {el: root,setValue:(v)=>{
+        code.innerHTML = v;
+    }}
+}
+
+function recompile(){
+    impsmap.setValue(show_imps_map(schemafield.el.value));
+    tscode.setValue(generate_ts(schema));
+    rustcode.setValue(generate_rust(schema));
+    hljs.highlightAll({ ignoreUnescapedHTML: true });
+}
+
+window.recompile = recompile;
+
+async function main(){
+    await init();
+
+    document.getElementById("outlist").append(impsmap.el,rustcode.el,tscode.el)
+    schemafield.setValue(schemafield.el.value)
+    recompile();
+}
+main()
---
+++ src/lib.rs
@@ -0,0 +1,80 @@
+
+use std::{panic, fmt::Write};
+
+use smith_codegen::Language;
+use smith_core::SmithType;
+use wasm_bindgen::prelude::*;
+
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// allocator.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[wasm_bindgen(start)]
+pub fn init(){
+    panic::set_hook(Box::new(console_error_panic_hook::hook));
+}
+
+#[wasm_bindgen]
+extern {
+    fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet() {
+    alert("Hello, smith-webdemo!");
+}
+
+#[wasm_bindgen]
+pub fn generate_ts(schema: &str) -> Result<String, String>{
+    smith_codegen::generate_lang(schema, Language::TypeScript)
+}
+
+#[wasm_bindgen]
+pub fn generate_rust(schema: &str) -> String{
+    let s = smith_codegen::generate_rust(schema);
+    let file = syn::parse_file(&s.to_string()).unwrap();
+    return prettyplease::unparse(&file)
+}
+
+#[wasm_bindgen]
+pub fn show_imps_map(schema: &str) -> String{
+    let gen = smith_core::compile(schema).generics;
+    fn format_vec(v: Vec<SmithType<String>>) -> String{
+        let mut s = String::new();
+        for v in v{
+            let mut sbuff = String::new();
+            v.write_self(&mut sbuff);
+            s.push_str(&format!("{},", sbuff))
+        }
+        s.pop();
+        return s
+    }
+    let mut s = String::new();
+    _=s.write_str("{\n");
+    for (imp, (gen,_)) in gen{
+        _=s.write_str(&format!("  \"{}\": \n", imp));
+        for (keytype, (_,version)) in gen{
+            let mut s2 = String::new();
+            match version.typ(){
+                smith_core::parser::ASTRootType::Struct(s) => {
+                    _=s2.write_str(&format!("{:?}",s.fields));
+                },
+                smith_core::parser::ASTRootType::Enum(s) => {
+                    let mut svar = String::new();
+                    for var in &s.variants{
+                        _=svar.write_str(&format!("{}{}, ",var.0, if let Some(s) = &var.1{format!("({})",format_vec(vec![s.clone()]))}else{String::new()}));
+                    }
+                    svar.pop();
+                    svar.pop();
+                    _=s2.write_str(&format!("[{svar}]"));
+                },
+            }
+
+            _=s.write_str(&format!("      [{}] => {}\n", format_vec(keytype),s2));
+        }
+    }
+    _=s.write_str("}");
+    return s
+}
